baa59805cbc6b078d2991d0b1f0469348e23b8c2
xzf <xvzhifeng@126.com>
2022-07-26 03:33:14 +0800

    add

--- src/js/view.js
+++ src/js/view.js
@@ -8,13 +8,13 @@ let client = null
 //     {id:2, value:300, isStart:false, workTimer: null}
 // ]
 
-function progress_load(n){
-    if (n==0) {alert("下载完成")};
-    var progress=document.getElementById("progress");
-    n=n-1;
-    task=100-n;
-    progress.value=task;
-    setTimeout("load("+n+")",100);
+function progress_load(n) {
+    if (n == 0) { alert("下载完成") };
+    var progress = document.getElementById("progress");
+    n = n - 1;
+    task = 100 - n;
+    progress.value = task;
+    setTimeout("load(" + n + ")", 100);
 }
 
 
@@ -34,16 +34,16 @@ function startWork(id, timeValue) {
 }
 
 function updateProgress(id, value) {
-    console.log(value)
+    console.log("id " + value)
     const progress = document.getElementById(`exec-task-progress-${id}`)
-    progress.value=value;
+    progress.value = value;
 }
 
 function initProgress(id, value) {
     console.log(id)
     const progress = document.getElementById(`exec-task-progress-${id}`)
     console.log(progress.max)
-    if(progress.max == 123.321) {
+    if (progress.max == 123.321) {
         console.log("init");
         progress.max = value
     }
@@ -55,21 +55,21 @@ function updateTime(id, ms, index) {
     const timerContainer = document.getElementById(`exec-task-time-${id}`)
     const s = (ms / 1000).toFixed(0)
     const ss = s % 60
-    const mm = (s / 60).toFixed(0)
+    const mm = parseInt(s / 60)
     updateProgress(id, s)
     timerContainer.innerText = `${mm.toString().padStart(2, 0)}: ${ss.toString().padStart(2, 0)}`
 }
 
-let  notifiF = async (id) => {
+let notifiF = async (id) => {
     const res = await new Promise((resolve, reject) => {
-        const notification = new Notification(`任务${id}结束`,{
+        const notification = new Notification(`任务${id}结束`, {
             body: '是否开始休息'
         })
         notification.onclick = () => {
             console.log("click notification");
             resolve('rest');
         }
-        notification.onclose = ()=>{
+        notification.onclose = () => {
             resolve('work')
         }
     })
@@ -92,55 +92,56 @@ async function notification(id) {
 }
 
 let updateData = async (data) => {
-    if(client == null) {
+    if (client == null) {
         client = await getClient()
     }
     let update_cmd = `update nct_timer
                         set remaninder_time = ${data.remaninder_time}
                         where id = ${data.id}`
     let res = await client.awaitQuery(update_cmd)
-    console.log('更新 :' + res)
+    console.log(`update ${res}`)
 }
 
 let deleteData = async (id) => {
     document.getElementById(`exec-task-${id}`).remove()
     s_tool.remove(id)
-    if(client == null) {
+    if (client == null) {
         client = await getClient()
     }
     let update_cmd = `update nct_timer
                         set status = 0
                         where id = ${id}`
     let res = await client.awaitQuery(update_cmd)
-    console.log('delete :' + res)
+    console.log(`'delete :' + ${res}`)
 }
 
 // 启动Task
 let task_start = (id, value) => {
-    console.log(t_tool.status)
-    for(let i=0;i<t_tool.status.length;i++) {
-        if(t_tool.status[i].id == id) {
-            if(t_tool.status[i].isStart) {
-                t_tool.status[i].workTimer.pause();
-                updateData(t_tool.status[i])
-                t_tool.status[i].isStart = false;
-            } else if (t_tool.status[i].workTimer != null) {
-                t_tool.status[i].isStart = true;
-                t_tool.status[i].workTimer.start(t_tool.status[i].remaninder_time);
+    console.log(s_tool.status)
+    for (let i = 0; i < s_tool.status.length; i++) {
+        if (s_tool.status[i].id == id) {
+            if (s_tool.status[i].isStart) {
+                s_tool.status[i].workTimer.pause();
+                console.log(`updateData`)
+                updateData(s_tool.status[i])
+                s_tool.status[i].isStart = false;
+            } else if (s_tool.status[i].workTimer != null) {
+                s_tool.status[i].isStart = true;
+                s_tool.status[i].workTimer.start(s_tool.status[i].remaninder_time);
             } else {
-                t_tool.status[i].isStart = true;
-                t_tool.status[i].workTimer = new Timer({
+                s_tool.status[i].isStart = true;
+                s_tool.status[i].workTimer = new Timer({
                     ontick: (timeValue) => {
                         console.log('onclick')
                         updateTime(id, timeValue, i)
-                        t_tool.status[i].remaninder_time = (timeValue / 1000).toFixed(0);
+                        s_tool.status[i].remaninder_time = (timeValue / 1000).toFixed(0);
                     }, onend: () => {
                         console.log('onend')
                         notification(id)
                         deleteData(id)
                     }
                 });
-                t_tool.status[i].workTimer.start(t_tool.status[i].remaninder_time);
+                s_tool.status[i].workTimer.start(s_tool.status[i].remaninder_time);
             }
         }
     }
@@ -148,37 +149,40 @@ let task_start = (id, value) => {
 }
 
 let generateTaskView = (data) => {
-    if(document.getElementById(`exec-task-${data.id}`)) {
-        // let demo1 = document.getElementById(`exec-task-${data.id}`);
-        // demo1.setAttribute("id", `exec-task-${data.id}`)
-        // demo1.setAttribute("onclick",`task_start(${data.id}, ${data.remaninder_time})`)
-        // let div = demo1.getElementsByTagName("div")
-        // let name = div[0]
-        // let time = div[1]
-        // let progress = div[2]
-        // name.setAttribute("id",`exec-task-name-${data.id}`)
-        // name.innerHTML = data.task_name
-        // time.setAttribute("id",`exec-task-time-${data.id}`)
-        // progress.setAttribute("id",`exec-task-progress-${data.id}`)
+    if (document.getElementById(`exec-task-${data.id}`)) {
+        let demo1 = document.getElementById(`exec-task-${data.id}`);
+        let div = demo1.getElementsByTagName("div")
+        let name = div[0]
+        let time = div[1]
+        let progress = div[2].getElementsByTagName('progress')
+        name.setAttribute("id", `exec-task-name-${data.id}`)
+        name.innerHTML = data.task_name
+        time.setAttribute("id", `exec-task-time-${data.id}`)
+        time.innerHTML = `${parseInt(data.remaninder_time / 60).toString().padStart(2, 0)} : ${(data.remaninder_time % 60)
+            .toString().padStart(2, 0)} `
+        progress[0].setAttribute("max", data.interval_time)
+        progress[0].setAttribute("value", data.remaninder_time)
         console.log("该元素已经存在，无需添加")
     } else {
         let demo = document.getElementById("exec-task-demo");
         let demo1 = demo.cloneNode(true);
         demo1.setAttribute("id", `exec-task-${data.id}`)
-        demo1.setAttribute("onclick",`task_start(${data.id}, ${data.remaninder_time})`)
+        demo1.setAttribute("onclick", `task_start(${data.id}, ${data.remaninder_time})`)
         let div = demo1.getElementsByTagName("div")
         let name = div[0]
         let time = div[1]
-        let progress = div[2]
-        name.setAttribute("id",`exec-task-name-${data.id}`)
+        let progress = div[2].getElementsByTagName('progress')
+        name.setAttribute("id", `exec-task-name-${data.id}`)
         name.innerHTML = data.task_name
-        time.setAttribute("id",`exec-task-time-${data.id}`)
-        time.innerHTML = `${data.remaninder_time%60} : ${(data.remaninder_time/60).toFixed(0)}`
-        progress.setAttribute("id",`exec-task-progress-${data.id}`)
-        progress.setAttribute("max",data.interval_time)
+        time.setAttribute("id", `exec-task-time-${data.id}`)
+        time.innerHTML = `${parseInt(data.remaninder_time / 60).toString().padStart(2, 0)} : ${(data.remaninder_time % 60)
+            .toString().padStart(2, 0)} `
+        progress[0].setAttribute("id", `exec-task-progress-${data.id}`)
+        progress[0].setAttribute("max", data.interval_time)
+        progress[0].setAttribute("value", data.remaninder_time)
         document.getElementById("view-task").appendChild(demo1);
     }
-    
+
     // demo1.onclick = task_start(id, value)
 }
 
@@ -187,14 +191,27 @@ let updateStatus = async () => {
     // 执行数据库操作
     let result = await client.awaitQuery(select_cmd)
     s_tool.update(result)
+    console.log(s_tool.set_process)
+    if (s_tool.get_is_change()) {
+        for (let i = 0; i < s_tool.status.length; i++) {
+            console.log(s_tool.status[i].id)
+            if (s_tool.is_change.has(s_tool.status[i].id)) {
+                generateTaskView(s_tool.status[i])
+                s_tool.set_is_change(s_tool.status[i].id)
+            }
+        }
+        s_tool.set_is_change(false)
+        console.log(s_tool.is_change)
+    }
+    console.log(s_tool.status)
 }
 
 let start = async () => {
-    if(client == null) {
+    if (client == null) {
         client = await getClient()
     }
-    
-    var t2 = window.setInterval( async function() {
+
+    var t2 = window.setInterval(async function () {
         // let select_cmd = `select * from nct_timer where status=1`
         // // 执行数据库操作
         // let result = await client.awaitQuery(select_cmd)
@@ -202,9 +219,10 @@ let start = async () => {
         //     console.log(result[i].id)
         //     generateTaskView(result[i].id, result[i].interval_time)
         // }
+
         updateStatus()
-        console.log(result)
+        // console.log(result)
         console.log('每隔1秒钟执行一次')
-        },100000)
+    }, 10000)
 }
 start()
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,9 +1,10 @@
 let getClient = require('./mysqlpool')
 let client = getClient()
+let is_change = new Set()
 
 let status = [
-    { id: 1, task_name: "t1", interval_time: 30, remaninder_time: 30, isStart: false, workTimer: null },
-    { id: 2, task_name: "t2", interval_time: 300, remaninder_time: 300, isStart: false, workTimer: null }
+    // { id: 1, task_name: "t1", interval_time: 30, remaninder_time: 30, isStart: false, workTimer: null },
+    // { id: 2, task_name: "t2", interval_time: 300, remaninder_time: 300, isStart: false, workTimer: null }
 ]
 let set_process = new Set()
 
@@ -12,18 +13,47 @@ let update = (result) => {
         if(!set_process.has(result[i].id)) {
             status.push({...result[i],workTimer: null})
             set_process.add(result[i].id)
+            is_change.add(result[i].id);
+            console.log("add status")
             // generateTaskView(result[i])
-        } 
+        } else {
+            for(let j =0;j<status.length;j++) {
+                if(result[i].id == status[j].id && result[i].remaninder_time < status[j].remaninder_time) {
+                    is_change.add(result[i].id);
+                    console.log("change status")
+                    console.log(`result[i].remaninder_time ${result[i].remaninder_time} != status[j].remaninder_time
+                    ${status[j].remaninder_time} ${status}`)
+                }
+            }
+        }
         console.log(result[i].id)
     }
 }
 
 let remove = (id) => {
-    status.map((s) => {
-        if(s.id != id) return s;
+    console.log(id)
+    status = status.filter((s) => {
+        return s.id != id;
     })
+}
+
+let get_is_change  = () => {
+    if(is_change.length != 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let set_is_change = (v) => {
+    is_change.delete(v)
 }
 
 
 module.exports.status = status
 module.exports.set_process = set_process
+module.exports.update = update
+module.exports.set_is_change = set_is_change
+module.exports.get_is_change = get_is_change
+module.exports.is_change = is_change
+module.exports.remove = remove
